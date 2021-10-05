8c1bc9c3f765fcd38469991fd7eb1105327b49a3
artemternovenko <artem.inkaz@gmail.com>
2021-10-05 17:37:26 +0600

    [ADDED] Add Scope, Clean Architecture

--- app/src/main/java/ui/smartpro/cleanarchgeekbrains/di/AppModule.kt
+++ app/src/main/java/ui/smartpro/cleanarchgeekbrains/di/AppModule.kt
@@ -6,6 +6,7 @@ import ui.smartpro.cleanarchgeekbrains.model.repository.RepositoryImplementation
 import ui.smartpro.cleanarchgeekbrains.model.repository.RepositoryImplementationLocal
 import ui.smartpro.cleanarchgeekbrains.model.repository.RepositoryLocal
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import ui.smartpro.repository.api.RetrofitModule
 import ui.smartpro.model.data.ResponseItem
@@ -13,12 +14,12 @@ import ui.smartpro.cleanarchgeekbrains.model.datasource.RetrofitImplementation
 import ui.smartpro.cleanarchgeekbrains.model.datasource.RoomDataBaseImplementation
 import ui.smartpro.cleanarchgeekbrains.storage.Database
 import ui.smartpro.cleanarchgeekbrains.ui.translate.Interactor
+import ui.smartpro.cleanarchgeekbrains.ui.translate.TranslateFragment
 import ui.smartpro.cleanarchgeekbrains.ui.translate.TranslateViewModel
 
 val appModule = module {
 
     single { RetrofitModule.create() }
-    single { Interactor(get(), get()) }
     single { Room.databaseBuilder(get(), Database::class.java, "HistoryDB").build() }
     single { get<Database>().dao() }
     single<Repository<List<ResponseItem>>> { RepositoryImplementation(RetrofitImplementation()) }
@@ -27,5 +28,8 @@ val appModule = module {
                 RoomDataBaseImplementation(get())
         )
     }
-    viewModel { TranslateViewModel(get()) }
+    scope(named<TranslateFragment>()) {
+        scoped { Interactor(get(), get()) }
+        viewModel { TranslateViewModel(get()) }
+    }
 }
